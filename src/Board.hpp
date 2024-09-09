779f0d2d0f77ff516d23544323294245e8997b6c
Joshua Shim <joshua.shim2004@gmail.com>
2024-09-09 14:30:54 +1000

    began work on the header file for the Board. Chose board implementation as 12 bitboards

---
+++ src/Board.hpp
@@ -0,0 +1,16 @@
+#ifndef Board_HPP_
+#define Board_HPP_
+
+#include <array>
+typedef uint64_t U64;
+
+
+class Board {
+    
+    public:
+
+    private:
+    std::array<U64, 20> myBitboards;
+};
+
+#endif /* Board_HPP_ */
