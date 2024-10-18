0d81c560445c8a29410ecedbb39c4daa03a736bc
Joshua Shim <joshua.shim2004@gmail.com>
2024-10-18 18:03:46 +1100

    implemented move generation for bishops and kings

--- src/engine/Board.cpp
+++ src/engine/Board.cpp
@@ -91,7 +91,7 @@ std::list<Board> Board::generate_white_moves() {
         }
         // add the next move to possible moves
         possible_moves.push_back(next_move);
-        single_push &= -single_push;
+        single_push ^= pawn_move;
         promotion = false;
     }
     // double push
@@ -105,7 +105,7 @@ std::list<Board> Board::generate_white_moves() {
         next_move.set_white_pawns(get_white_pawns() ^ (pawn_move >> 16));
         next_move.set_white_pawns(next_move.get_white_pawns() | pawn_move);
         possible_moves.push_back(next_move);
-        double_push &= -double_push;
+        double_push ^= pawn_move;
     }
     // pawn attack
     U64 left_pawn_attack = (get_white_pawns() << 9) & NOT_FILE_A & black_occupied;
@@ -123,7 +123,7 @@ std::list<Board> Board::generate_white_moves() {
             next_move.set_white_pawns(next_move.get_white_pawns() | pawn_move);
         }
         possible_moves.push_back(next_move);
-        left_pawn_attack &= -left_pawn_attack;
+        left_pawn_attack ^= pawn_move;
         promotion = false;
         
     }
@@ -142,7 +142,7 @@ std::list<Board> Board::generate_white_moves() {
             next_move.set_white_pawns(next_move.get_white_pawns() | pawn_move);
         }
         possible_moves.push_back(next_move);
-        right_pawn_attack &= -right_pawn_attack;
+        right_pawn_attack ^= pawn_move;
         promotion = false;
     }
     // en passant
@@ -164,7 +164,7 @@ std::list<Board> Board::generate_white_moves() {
         // change white pawns
         next_move.set_white_pawns((get_white_pawns() | pawn_move) ^ (pawn_move >> 9));
         possible_moves.push_back(next_move);
-        left_enpassant &= -left_enpassant;
+        left_enpassant ^= pawn_move;
     }
     U64 right_enpassant = (get_white_pawns() << 7) & NOT_FILE_H & ~(white_occupied | black_occupied);
     for (auto file : FILES) {
@@ -183,13 +183,120 @@ std::list<Board> Board::generate_white_moves() {
         // change white pawns
         next_move.set_white_pawns((get_white_pawns() | pawn_move) ^ (pawn_move >> 7));
         possible_moves.push_back(next_move);
-        right_enpassant &= -right_enpassant;
+        right_enpassant ^= pawn_move;
     }
     // knight
+    U64 original_knight_positions = get_white_knights();
+    const U64 FILE_AB = FILE_A | (FILE_A << 1);
+    const U64 FILE_GH = FILE_H | (FILE_H >> 1);
+    while (original_knight_positions) {
+        auto knight = original_knight_positions & - original_knight_positions;
+        // get possible moves for this knight
+        U64 l1 = (knight >> 1) & ~FILE_H;
+        U64 l2 = (knight >> 2) & ~FILE_GH;
+        U64 r1 = (knight << 1) & ~FILE_A;
+        U64 r2 = (knight << 2) & ~FILE_AB;
+        U64 moves = ((l1 << 16) | (l1 >> 16) | (r1 << 16) | (r1 >> 16) | (l2 <<  8) | (l2 >>  8) | (r2 <<  8) | (r2 >>  8)) & ~white_occupied;
+        while (moves) {
+            auto next_move = *this;
+            auto knight_move = moves & -moves;
+            // move knight
+            next_move.set_white_knights((get_white_knights() ^ knight) | knight_move);
+            // remove any black pieces in destination
+            for (auto i = 6; i < 12; ++i) {
+                if (next_move.bitboards[i] & knight_move) {
+                    next_move.bitboards[i] ^= knight_move;
+                    break;
+                }
+            }
+            moves ^= knight_move;
+            possible_moves.push_back(next_move);
+        }
+        original_knight_positions ^= knight;
+    }
+    // king because it only moves once
+    U64 left = (get_white_king() >> 1) & ~FILE_H;
+    U64 right = (get_white_king() << 1) & ~FILE_A;
+    U64 up = (get_white_king() << 8) & ~RANK_1;
+    U64 down = (get_white_king() >> 8) & ~RANK_8;
+    U64 upLeft = (get_white_king() << 7) & ~FILE_H & ~RANK_1;
+    U64 upRight = (get_white_king() << 9) & ~FILE_A & ~RANK_1;
+    U64 downLeft = (get_white_king() >> 9) & ~FILE_H & ~RANK_8;
+    U64 downRight = (get_white_king() >> 7) & ~FILE_A & ~RANK_8;
+    U64 king_moves = (left | right | up | down | upLeft | upRight | downLeft | downRight) & ~white_occupied;
+    while (king_moves) {
+        auto next_move = *this;
+        auto king_move = king_moves & -king_moves;
+        // move king
+        next_move.set_white_king(king_move);
+        for (auto i = 6; i < 12; ++i) {
+            if (next_move.bitboards[i] & king_move) {
+                next_move.bitboards[i] ^= king_move;
+                break;
+            }
+        }
+        king_moves ^= king_move;
+        possible_moves.push_back(next_move);
+    }
+    // after the next 3 i should be done with move generation (hopefully no bugs)
     // bishop
+    U64 og_bishop_positions = get_white_bishops();
+    while (og_bishop_positions) {
+        U64 bishop = og_bishop_positions & -og_bishop_positions;
+        U64 bishop_ptr = bishop;
+        U64 bishop_moves = 0;
+        // NW not file h and rank 1
+        while ((bishop_ptr << 9) & ~FILE_H & ~RANK_1) {
+            bishop_ptr <<= 9;
+            if (bishop_ptr & white_occupied) break;
+            bishop_moves |= bishop_moves;
+            if (bishop_ptr & black_occupied) break;
+        }
+        bishop_ptr = bishop;
+        // NE
+        while ((bishop_ptr << 7) & ~FILE_A & ~RANK_1) {
+            bishop_ptr <<= 7;
+            if (bishop_ptr & white_occupied) break;
+            bishop_moves |= bishop_moves;
+            if (bishop_ptr & black_occupied) break;
+        }
+        bishop_ptr = bishop;
+        // SW
+        while ((bishop_ptr >> 7) & ~FILE_H & ~RANK_8) {
+            bishop_ptr >>= 7;
+            if (bishop_ptr & white_occupied) break;
+            bishop_moves |= bishop_moves;
+            if (bishop_ptr & black_occupied) break;
+        }
+        bishop_ptr = bishop;
+        // SE
+        while ((bishop_ptr >> 9) & ~FILE_A & ~RANK_8) {
+            bishop_ptr >>= 9;
+            if (bishop_ptr & white_occupied) break;
+            bishop_moves |= bishop_moves;
+            if (bishop_ptr & black_occupied) break;
+        }
+        og_bishop_positions ^= bishop;
+        // do collected moves
+        while (bishop_moves) {
+            auto next_move = *this;
+            auto bishop_move = bishop_moves & -bishop_moves;
+            // move the bishop
+            next_move.set_white_bishops((get_white_bishops() ^ bishop) | bishop_move);
+            if (bishop_move & black_occupied) {
+                for (auto i = 6; i < 12; ++i) {
+                    next_move.bitboards[i] ^= bishop_move;
+                    break;
+                }
+            }
+            possible_moves.push_back(next_move);
+            bishop_moves ^= bishop_move;
+        }
+    }
+    
+    
+    
     // rook
     // queen
-    // king 
-    // do king, knight, 
 
 }
